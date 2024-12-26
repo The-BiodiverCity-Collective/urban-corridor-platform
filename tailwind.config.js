c8c307c0d377aab2401facc880933435fe8ad328
Paul Hoekman <p@ulhoekman.com>
2024-12-26 19:00:18 +0100

    Tailwind npm setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',   // Tailwind will scan all HTML files inside the templates folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
