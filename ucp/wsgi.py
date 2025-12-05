6112cd4b3b8d036dda96e22d49751dd7f3330945
Paul Hoekman <p@ulhoekman.com>
2025-12-05 21:29:04 +0200

    Tweak server config

--- ucp/wsgi.py
+++ ucp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fcc project.
+WSGI config for UCP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fcc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucp.settings')
 
 application = get_wsgi_application()
